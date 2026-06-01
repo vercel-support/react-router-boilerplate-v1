33c2e2b2d126fff7cb7047eccdfccfed8a823671
Vikhyath S <49331452+vikhyathdevadiga@users.noreply.github.com>
2026-06-01 14:45:14 +0530

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,6 +5,6 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  prerender: true,
+  prerender: false,
   presets: [vercelPreset()],
 } satisfies Config;
